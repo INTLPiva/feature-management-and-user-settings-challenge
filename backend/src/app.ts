669889fd7120163073c13d727ba1210587074b4d
INTLPiva <lucas.piva@gec.inatel.br>
2026-01-16 11:09:55 -0300

    feat: setup fastify server and env config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,19 @@
-import fastify from "fastify";
+import Fastify from "fastify";
+import { ZodError } from "zod";
+import { registerRoutes } from "./application/routes";
 
-export const app = fastify();
+export const app = Fastify();
+
+registerRoutes(app);
+
+app.setErrorHandler(async (error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  console.error(error);
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
